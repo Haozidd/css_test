5d00a96403d8db10efa99e467a3acd7aa7c32ea5
Haozidd <285626941@qq.com>
2023-01-13 17:08:32 +0800

    bubble-cmpl

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,42 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="container">
+    <Bubble/>
+
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Bubble from "@/components/Bubble";
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Bubble
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+*{
+  padding: 0;
+  margin: 0;
+}
+body{
+  width: 100%;
+  height: 100vh;
+  background:
+      linear-gradient(150deg,rgba(0,0,0,.1),rgba(0,0,0,.1)),
+      linear-gradient(150deg,plum,lemonchiffon);
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+#container {
+  width: 200px;
+  height: 400px;
+  position: relative;
+
 }
 </style>
---
+++ src/components/Bubble.vue
@@ -0,0 +1,81 @@
+<template>
+  <div id="bubble-container">
+    <div id="bubble"></div>
+    <div id="bubble-shadow"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Bubble"
+}
+</script>
+
+<style scoped lang="scss">
+#bubble-container{
+  display: flex;
+  flex-flow: column nowrap;
+  align-items: center;
+
+  width: 100%;
+  height: 70%;
+}
+#bubble{
+  width: 200px;
+  height: 200px;
+
+  background: radial-gradient(
+          circle at 75% 30%,
+          white 6px,
+          rgb(239, 135, 182) 7%,
+          skyblue 60%,
+          plum 100%
+  );
+  border-radius: 50%;
+  box-shadow: inset 0 0 20px #fff,
+  inset 10px 0 46px #eaf5fc,
+  inset 80px 10px 60px #eacde6,
+  inset -20px -60px 46px #f9f6de,
+  inset 0px 50px 140px #f9f6de,
+  0px 0px 30px white;
+
+  animation: bubble 4s ease-in-out infinite;
+}
+#bubble-shadow{
+  background: gray;
+  opacity:.6;
+  margin-top: 20px;
+  width: 150px;
+  height: 40px;
+  border-radius: 50%;
+
+  animation: bubble-shadow 4s ease-in-out infinite;
+
+}
+@keyframes bubble-shadow {
+  0%,100%{
+    transform: scale(.8);
+  }
+  50%{
+    transform: scale(1.4);
+    opacity: .3;
+  }
+}
+
+@keyframes bubble{
+  0%,100%{
+    transform: translateY(0);
+  }
+  50%{
+    transform: translateY(-80px);
+    box-shadow: inset 0 0 20px #fff,
+    inset -10px 0 46px #eaf5fc,
+    inset -40px -10px 60px #eacde6,
+    inset -0px 60px 46px #f9f6de,
+    inset 0px -50px 140px #f9f6de,
+    0px 0px 90px white;
+  }
+}
+
+
+</style>
